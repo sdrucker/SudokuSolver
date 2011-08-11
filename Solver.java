690d7851d35a8b6f46ca0386cfb59a024dbb9ecc
Dani Dickstein <danidickstein@yahoo.com>
2011-08-11 11:17:25 -0400

    Solver compiles but is still not fully operational

--- Solver.java
+++ Solver.java
@@ -16,66 +16,77 @@ public class Solver {
         solve();
     }
     
-	/**
-	 * Actually performs the solving
-	 * >> This method is incomplete, and will be developed as the application progresses.
-	 */
-	public void solve() {
-		do {
-			for (int c = 0; c < 9; c++) {
-				for (int r = 0; r < 9; r++) {
-					if (!(board.getSlot(c,r).isSingle()))
-						singlify(c,r);
-				}
-			}
-		} while (!singlified());
-	}
+    /**
+     * Actually performs the solving
+     * >> This method is incomplete, and will be developed as the application progresses.
+     */
+    public void solve() {
+        do {
+            for (int c = 0; c < 9; c++) {
+                for (int r = 0; r < 9; r++) {
+                    if (!(board.getSlot(c,r).isSingle()))
+                        singlify(c,r);
+                }
+            }
+        } while (!singlified());
+    }
 
     /**
      * Checks whether all of the cells that can be evaluated by basic methods have been simplified as much as possible
      */
     public boolean singlified() {
-		for (int c = 0; c < 9; c++) {
-			for (int r = 0; r < 9; r++) {
-				if (!singlify(c,r))
-					return false;
-			}
-		}
-		return true;
+        for (int c = 0; c < 9; c++) {
+            for (int r = 0; r < 9; r++) {
+                if (!singlify(c,r))
+                    return false;
+            }
+        }
+        return true;
     }
     
-	/**
-	 * Simplifies a slot as method, trying to make it "single" if possible
-	 * @return Whether any removal of the slot occurred
- 	 */
-	public boolean singlify(int c, int r) {
-		int[] box = board.getSquare(board.getSquareNumber(c,r));
-		int[] row = board.getRow(r);
-		int[] col = board.getCol(c);
-		for (int i=1;i<10;i++) {
-			if (contains(row,i) || contains(col,i) || contains(box,i)) {
-				boardgetSlot(c,r).remove(i);
-			}
-		}
-	}
+    /**
+     * Simplifies a slot as method, trying to make it "single" if possible
+     * @return Whether any removal of the slot occurred
+     */
+    public boolean singlify(int c, int r) {
+        Slot[] Sbox = board.getSquare(board.getSquareNumber(c,r));
+        Slot[] Srow = board.getRow(r);
+        Slot[] Scol = board.getCol(c);
+        int[] box = new int[9];
+        int[] row = new int[9];
+        int[] col = new int[9];
+        for (int i = 0; i < 9; i++) {
+            box[i] = Sbox[i].value();
+            row[i] = Srow[i].value();
+            col[i] = Scol[i].value();
+        }
+        
+        for (int i=1;i<10;i++) {
+            if (contains(row,i) || contains(col,i) || contains(box,i)) {
+                board.getSlot(c,r).remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private boolean contains(int[] arr, int key) {
-		return Arrays.asList(wrap(arr)).contains((Integer)key);
-	}
-	
-	private Integer[] wrap(int[] arr) {
-		Integer[] wrapperArray = new Integer[arr.length];
-		for (int i = 0; i < arr.length; i++) {
-			wrapperArray[i]=(Integer)arr[i];
-		}
-		return wrapperArray;
-	}
-	
-	private int[] unwrap(Integer[] arr) {
-		int[] primitiveArray = new int[arr.length];
-		for (int i = 0; i < arr.length; i++) {
-			primitiveArray[i]=(int)arr[i];
-		}
-		return primitiveArray;
-	}
+    private boolean contains(int[] arr, int key) {
+        return Arrays.asList(wrap(arr)).contains((Integer)key);
+    }
+    
+    private Integer[] wrap(int[] arr) {
+        Integer[] wrapperArray = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            wrapperArray[i]=(Integer)arr[i];
+        }
+        return wrapperArray;
+    }
+    
+    private int[] unwrap(Integer[] arr) {
+        int[] primitiveArray = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            primitiveArray[i]=(int)arr[i];
+        }
+        return primitiveArray;
+    }
 }
